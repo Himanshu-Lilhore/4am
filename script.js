13be66638cd08a544d2e764dc3512bfeb6c440ab
Himanshu-Lilhore <himanshulilhore@gmail.com>
2024-01-25 05:04:17 +0530

    Commit_25-01-2024( 5:04)

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 let totalVids = 131;
+let isLooping = true;
 
 let refreshBtn = document.getElementById("refreshBtn");
 let vidHolder = document.getElementById("vidHolder");
 let vid = document.getElementById("vid");
 let playDiv = document.querySelector('#play');
 let pauseDiv = document.querySelector('#pause');
+let loopToggleBtn = document.querySelector('#loopToggle');
+let playlist = document.querySelector('#playlist');
+let loop = document.querySelector('#loop');
 
 refreshBtn.addEventListener('click', randVid);
 vidHolder.addEventListener('click', playpause);
+loopToggleBtn.addEventListener('click', toggleLoopMode);
+vid.addEventListener('loadedmetadata', onVideoLoadedMetadata);
+vid.addEventListener('timeupdate', checkIfVideoEnded);
+vid.addEventListener('ended', playNext);
 
 randVid();
 
@@ -15,6 +23,19 @@ function randVid(){
     let randNum = getRandomNumber(1,totalVids);
     vid.setAttribute("src", "./videos/"+randNum+".mp4");
     console.log(randNum);
+}
+
+function checkIfVideoEnded() {
+    if (vid.currentTime >= vid.duration - 0.5) {
+        playNext();
+    }
+}
+
+function onVideoLoadedMetadata() {
+    console.log("Video duration:", vid.duration);
+    if (vid.duration !== Infinity) {
+        vid.addEventListener('timeupdate', checkIfVideoEnded);
+    }
 }
 
 
@@ -39,7 +60,23 @@ function playpause(){
 }
 
 
+function toggleLoopMode(){
+    loop.classList.toggle("hidden");
+    playlist.classList.toggle("hidden");
+    isLooping = !isLooping;
+    console.log("toggleed play mode : " + isLooping);
+}
 
+function playNext(){
+    if(!isLooping){
+        console.log("not looping");
+        randVid();
+    }
+    else{
+        vid.play();
+        console.log("looping");
+    }
+}
 
 
 // window.onload = function () 
